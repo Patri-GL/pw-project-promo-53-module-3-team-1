41fec89afe909d36d2c193e8ffa8fdfaf7bb24ff
cris <christty_92@hotmail.com>
2025-06-04 13:29:02 +0200

    locals

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import "../styles/App.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Footer from "./Layout/Footer";
 import Header from "./Layout/Header";
 import Hero from "./Layout/Hero";
@@ -7,64 +7,48 @@ import Card from "./Projects/Card";
 import Form from "./Projects/Form";
 
 function App() {
-  const [data, setData] = useState({
-    name: "Elegant Workspace",
-    slogan: "Diseños Exclusivos",
-    repo: "http://localhost:5173/#",
-    demo: "http://localhost:5173/#",
-    technologies: "React JS - HTML - CSS",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione",
-    author: "Emmelie Bjôrklund",
-    job: "Full stack Developer",
+  const [data, setData] = useState(() => {
+    const savedData = localStorage.getItem("data");
+    return savedData ? JSON.parse(savedData) : {
+      name: "Elegant Workspace",
+      slogan: "Diseños Exclusivos",
+      repo: "http://localhost:5173/#",
+      demo: "http://localhost:5173/#",
+      technologies: "React JS - HTML - CSS",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione",
+      author: "Emmelie Bjôrklund",
+      job: "Full stack Developer",
+    };
   });
-  // const [nameProject, setNameProject] = useState("Elegant Workspace");
-  // const [sloganProject, setSloganProject] = useState("Diseños Exclusivos");
-  // const [repoProject, setRepoProject] = useState("http://localhost:5173/#");
-  // const [demoProject, setDemoProject] = useState("http://localhost:5173/#");
-  // const [technologies, setTechnologies] = useState("React JS - HTML - CSS");
-  // const [descProject, setDescProject] = useState(
-  //   "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione"
-  // );
-  // const [authorProject, setAuthorProject] = useState("Emmelie Bjôrklund");
-  // const [jobProject, setJobProject] = useState("Full stack Developer");
 
   const changeState = (field, value) => {
-    setData((prevData) => ({
+    setData((prevData) => {
+      const newData = {
       ...prevData,
+      
       [field]: value,
-    }));
-  };
+    };
+  
 
-  // const changeState = (field, value) => {
-  //   console.log("changestate(", field, value, ")");
+  
+    localStorage.setItem("data", JSON.stringify(newData));
 
-  //   if (field === "name") {
-  //     setData(value);
-  //   } else if (field === "slogan") {
-  //     setData(value);
-  //   } else if (field === "repo") {
-  //     setData(value);
-  //   } else if (field === "demo") {
-  //     setData(value);
-  //   } else if (field === "technologies") {
-  //     setData(value);
-  //   } else if (field === "desc") {
-  //     setData(value);
-  //   } else if (field === "author") {
-  //     setData(value);
-  //   } else if (field === "job") {
-  //     setData(value);
-  //   }
-  // };
-  //para añadir imágenes: https://github.com/Adalab/componente-react-de-foto-de-perfil
+    return newData;
+  });
+};
+
+
+  useEffect(() => {
+    console.log("Guardando en localStorage:", data);
+    localStorage.setItem("data", JSON.stringify(data));
+  }, [data]);
+
   return (
     <>
       <div className="container">
         <Header />
-
         <main className="main">
           <Hero />
-
           <Card
             name={data.name}
             slogan={data.slogan}
@@ -75,11 +59,13 @@ function App() {
             author={data.author}
             job={data.job}
           />
-          <Form changeState={changeState} />
+          <Form data={data} changeState={changeState} />
+ 
         </main>
         <Footer />
       </div>
     </>
   );
 }
+
 export default App;
